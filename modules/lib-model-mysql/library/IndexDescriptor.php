91cc7e3fdfb0637088ac38589db43effad1db3fb
Iqbal Fauzi <iqbalfawz@gmail.com>
2026-06-11 07:16:01 +0700

    Identify index unique by multiple column

--- modules/lib-model-mysql/library/IndexDescriptor.php
+++ modules/lib-model-mysql/library/IndexDescriptor.php
@@ -9,41 +9,49 @@ namespace LibModelMysql\Library;
 
 class IndexDescriptor
 {
-
-    static function describe(string $model, array $fields): ?array {
+    public static function describe(string $model, array $fields): ?array
+    {
         $sql = $model::putFrom('SHOW INDEXES (:from)');
         $indexes = $model::query($sql, 'write');
-        if(!$indexes)
+        if (!$indexes) {
             return null;
+        }
 
         $result = [];
 
-        foreach($indexes as $index){
+        foreach ($indexes as $index) {
             $name = $index->Key_name;
 
             // skip primary name
-            if($name === 'PRIMARY')
+            if ($name === 'PRIMARY') {
                 continue;
-
-            // is unique/primary key
-            if(isset($fields[$name])){
-                $is_unique  = $fields[$name]['attrs']['unique'] ?? false;
-                $is_primary = $fields[$name]['attrs']['primary_key'] ?? false;
-                if($is_unique || $is_primary)
-                    continue;
             }
 
-            if(!isset($result[$name])){
+            // is unique/primary key
+            if (isset($fields[$name])) {
+                $is_unique  = $fields[$name]['attrs']['unique'] ?? false;
+                $is_primary = $fields[$name]['attrs']['primary_key'] ?? false;
+                if ($is_unique || $is_primary) {
+                    continue;
+                }
+            }
+
+            if (!isset($result[$name])) {
+                $type = $index->Index_type;
+                if (!$index->Non_unique) {
+                    $type = 'UNIQUE';
+                }
                 $result[$name] = [
                     'name' => $name,
-                    'type' => $index->Index_type,
+                    'type' => $type,
                     'fields' => []
                 ];
             }
 
             $idx = [];
-            if($index->Sub_part)
+            if ($index->Sub_part) {
                 $idx['length'] = $index->Sub_part;
+            }
             $field = $index->Column_name;
 
             $result[$name]['fields'][$field] = $idx;
